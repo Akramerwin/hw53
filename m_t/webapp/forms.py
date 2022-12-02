7fd6aae358cb8853aba0779ede05b1c307f46c1e
akram < >
2022-12-02 16:43:48 +0600

    i deleted com

--- m_t/webapp/forms.py
+++ m_t/webapp/forms.py
@@ -4,12 +4,6 @@ from webapp.models import Todo
 from django.core.validators import BaseValidator, ValidationError
 from django.utils.deconstruct import deconstructible
 
-# class TodoForm(forms.Form):
-#     short_description = forms.CharField(label='short_description')
-#     description = forms.CharField(label='description', widget=forms.Textarea())
-#     status = forms.ModelChoiceField(queryset=Status.objects.all(), label='status', widget=widgets.Select)
-#     type = forms.ModelMultipleChoiceField(required=False, label='type', queryset=Type.objects.all(), widget=forms.CheckboxSelectMultiple)
-
 class TodoForm(forms.ModelForm):
 
     class Meta:
--- m_t/webapp/views.py
+++ m_t/webapp/views.py
@@ -64,9 +64,6 @@ class TodoCreate(FormView):
         return reverse('view', kwargs = {'pk': self.todo.pk})
 
     def form_valid(self, form):
-        # type = form.cleaned_data.pop('type')
-        # self.todo = Todo.objects.create(**form.cleaned_data)
-        # self.todo.type.set(type)
         self.todo = form.save()
         return super().form_valid(form)
 
@@ -92,20 +89,9 @@ class UpdateTodo(FormView):
         kwargs = super().get_form_kwargs()
         kwargs['instance'] = self.todo
         return kwargs
-    # def get_initial(self):
-    #     initial = {}
-    #     for key in 'short_description', 'description', 'status':
-    #         initial[key] = getattr(self.todo, key)
-    #     initial['type'] = self.todo.type.all()
-    #     return initial
+
 
     def form_valid(self, form):
-        # type = form.cleaned_data.pop('type')
-        # for key, value in form.cleaned_data.items():
-        #     if value is not None:
-        #         setattr(self.todo, key, value)
-        # self.todo.save()
-        # self.todo.type.set(type)
         self.todo = form.save()
         return super().form_valid(form)
 
